abb28758377f346e534118f884e637874574badc
Lionel <lionelhuizing@gmail.com>
2021-07-29 18:58:57 +0200

    Lesson 18

--- app.ts
+++ app.ts
@@ -4,7 +4,16 @@
 // } = {
 const person = {
     name: 'Lionel',
-    age: 38
+    age: 38,
+    hobbies: ['Sports', 'Cooking']
 };
 
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies){
+    console.log(hobby);
+    //console.log(hobby.map()); !!! ERROR map does not exist in type string!
+}
